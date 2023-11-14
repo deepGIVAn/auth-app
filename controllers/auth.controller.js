d76acc989e552ba2e42befc81a8a4d28f2a52253
deepGIVAn <88539737+deepGIVAn@users.noreply.github.com>
2023-11-14 15:19:02 +0530

    sign up

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,7 +1,8 @@
 import User from "../models/user.model.js";
 import bcryptjs from 'bcryptjs';
+import { errorHandler } from "../utils/error.js";
 
-export const signup = async (req,res)=>{
+export const signup = async (req,res,next)=>{
     const { username, email, password } = req.body;
     // if i just uses hash here so i need to define the await in front of the code line..
     const hashedPassword = bcryptjs.hashSync(password, 10);
@@ -11,6 +12,8 @@ export const signup = async (req,res)=>{
         await newUser.save();
         res.status(201).json({message:"User created successfully"});        
     } catch (error) {
-        res.status(500).json(error.message);
+        // res.status(500).json(error.message);
+        next(error);
+        // next(errorHandler(300,"Something went wrong!"));
     }
 }
--- index.js
+++ index.js
@@ -10,11 +10,21 @@ dotenv.config();
 const PORT = process.env.PORT || 3500;
 const app = express();
 
-app.use(morgan('tiny'));
+app.use(morgan("tiny"));
 app.use(express.json());
 
-app.use("/api/user",userRoutes);
-app.use("/api/auth",authRoutes);
+app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
+
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "Internal Server Error";
+  return res.status(statusCode).json({
+    success: false,
+    message,
+    statusCode,
+  });
+});
 
 mongoose
   .connect(process.env.MONGO)
